99712fed7d83e4add6c686537cc1e865b5f92a8b
adhilabu <adhilabacker@gmail.com>
2024-06-05 18:51:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/counterpoc/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.counterpoc
+
+import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.example.counterpoc.ui.theme.CounterpocTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val counterTextView: TextView = findViewById(R.id.counterTextView)
+        val incrementButton: Button = findViewById(R.id.incrementButton)
+
+        incrementButton.setOnClickListener {
+            counter++
+            counterTextView.text = counter.toString()
+        }
+    }
+}
